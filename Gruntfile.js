cb0b0154face350bb1264dec35c32477741d0aa6
Michael Cornelis <mishcozii@gmail.com>
2015-09-17 09:23:08 +0000

    various responsive fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,10 +10,10 @@ module.exports = function(grunt) {
 
         connect: {
             options: {
-                port: 9000,
+                port: 4000,
                 livereload: 35729,
                 // Change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
